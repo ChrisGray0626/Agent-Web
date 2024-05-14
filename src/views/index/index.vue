e8c4d68032904cd7a50387e15b64a8430ed90ea4
Chris <yuxiaohui0626@gmail.com>
2024-05-14 13:06:38 +0800

    - chore: remove the useless

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -6,9 +6,6 @@ import HelloWorld from "/src/components/HelloWorld.vue"
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img alt="Vite logo" class="logo" src="/vite.svg"/>
-    </a>
     <a href="https://vuejs.org/" target="_blank">
       <img alt="Vue logo" class="logo vue" src="/src/assets/vue.svg"/>
     </a>
