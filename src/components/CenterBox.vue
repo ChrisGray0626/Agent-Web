282d5d284b1d91a428e71aa91d2512cd34e3bd98
Chris <yuxiaohui0626@gmail.com>
2024-05-28 17:25:03 +0800

    - feat(autoLayout)

--- src/components/CenterBox.vue
+++ src/components/CenterBox.vue
@@ -6,12 +6,6 @@ import Flow from "/src/components/flow/index.vue";
 <template>
   <div class="container">
     <Flow />
-    <!--    <div>-->
-    <!--      <a href="https://vuejs.org/" target="_blank">-->
-    <!--        <img alt="Vue logo" class="logo vue" src="/src/assets/vue.svg" />-->
-    <!--      </a>-->
-    <!--    </div>-->
-    <!--    <HelloWorld msg="Agent-Web" />-->
   </div>
 </template>
 
--- src/components/flow/index.vue
+++ src/components/flow/index.vue
@@ -8,58 +8,159 @@ import { Control } from '@logicflow/extension';
 import "@logicflow/extension/lib/style/index.css";
 
 import {onMounted} from "vue";
+import {layoutGraphData} from "/src/utils/autoLayout/layout"
+import RootNode from "/src/utils/autoLayout/RootNode";
+import Edge from "/src/utils/autoLayout/Edge";
+import Node from "../../utils/autoLayout/Node.ts";
 
-const graphData = {
+const lf = new LogicFlow(<Definition>{
+  container: document.getElementById('container'),
+  stopScrollGraph: true,
+  stopZoomGraph: true,
+  isSilentMode: true,
+  // TODO width and height
+  width: 800,
+  height: 600,
+  grid: true,
+  plugins: [Control],
+});
+
+let graphData = {
   nodes: [
     {
-      id: "node_id_1",
-      type: "rect",
-      x: 100,
-      y: 100,
-      text: {x: 100, y: 100, value: '节点1'},
-      properties: {}
+      id: "node_id_1_1",
+      x: 200,
+      y: 200,
+      type: "center-node",
+      text: {value: '节点1-1'},
+      properties: {
+        "input": "input",
+        "output": "output"
+      }
     },
     {
-      id: "node_id_2",
-      type: "circle",
-      x: 200,
-      y: 300,
-      text: {x: 300, y: 300, value: '节点2'},
-      properties: {}
-    }
+      id: "node_id_2_1",
+      type: "sub-node",
+      text: {value: '节点2-1'},
+    },
+    {
+      id: "node_id_2_2",
+      type: "sub-node",
+      text: {value: '节点2-2'},
+    },
+    {
+      id: "node_id_3_1",
+      type: "sub-node",
+      text: {value: '节点3-1'},
+    },
+    {
+      id: "node_id_3_2",
+      type: "sub-node",
+      text: {value: '节点3-2'},
+    },
+    {
+      id: "node_id_3_3",
+      type: "sub-node",
+      text: {value: '节点3-3'},
+    },
+    {
+      id: "node_id_3_4",
+      type: "sub-node",
+      text: {value: '节点3-4'},
+    },
   ],
   edges: [
     {
-      id: "edge_id",
-      type: "polyline",
-      sourceNodeId: "node_id_1",
-      targetNodeId: "node_id_2",
-      text: {x: 139, y: 200, value: "连线"},
-      startPoint: {x: 100, y: 140},
-      endPoint: {x: 200, y: 250},
-      pointsList: [{x: 100, y: 140}, {x: 100, y: 200}, {x: 200, y: 200}, {x: 200, y: 250}],
-      properties: {}
-    }
+      id: "edge_id_1",
+      type: "mindmap-edge",
+      sourceNodeId: "node_id_1_1",
+      targetNodeId: "node_id_2_1",
+    },
+    {
+      id: "edge_id_2",
+      type: "mindmap-edge",
+      sourceNodeId: "node_id_1_1",
+      targetNodeId: "node_id_2_2",
+    },
+    {
+      id: "edge_id_3",
+      type: "mindmap-edge",
+      sourceNodeId: "node_id_2_2",
+      targetNodeId: "node_id_3_1",
+    },
+    {
+      id: "edge_id_4",
+      type: "mindmap-edge",
+      sourceNodeId: "node_id_2_2",
+      targetNodeId: "node_id_3_2",
+    },
+    {
+      id: "edge_id_5",
+      type: "mindmap-edge",
+      sourceNodeId: "node_id_2_2",
+      targetNodeId: "node_id_3_3",
+    },
+    {
+      id: "edge_id_6",
+      type: "mindmap-edge",
+      sourceNodeId: "node_id_2_2",
+      targetNodeId: "node_id_3_4",
+    },
   ]
 }
 
+
+
 onMounted(() => {
-  const lf = new LogicFlow(<Definition>{
-    container: document.getElementById('container'),
-    stopScrollGraph: true,
-    stopZoomGraph: true,
-    // TODO width and height
-    width: 800,
-    height: 600,
-    grid: true,
-    plugins: [Control],
-  });
+  lf.register(RootNode)
+  lf.register(Node)
+  lf.register(Edge)
   lf.render(graphData);
+  graphData = lf.getGraphData()
+  const { nodes, edges } = layoutGraphData(graphData)
+
+  const nodeIdMap =  nodes.reduce((acc, cur) => {
+    acc[cur.id] = cur
+    return acc
+  }, {})
+  // 处理edge弧线，保持其美观
+  edges.map((edge) => {
+    const {
+      sourceNodeId,
+      targetNodeId
+    } = edge
+    // const sModel = lf.getNodeModelById(sourceNodeId)
+    const tModel = lf.getNodeModelById(targetNodeId)
+    // console.log('targetNodeId', targetNodeId)
+    // console.log('tModel', tModel)
+    const startPoint = {
+      x: nodeIdMap[sourceNodeId].x,
+      y: nodeIdMap[sourceNodeId].y
+    }
+    const sJustPoint = {
+      x: nodeIdMap[targetNodeId].x - tModel.width / 2 - 100,
+      y: nodeIdMap[targetNodeId].y
+    }
+    const tJustPoint = {
+      x: nodeIdMap[targetNodeId].x - tModel.width / 2 - 100,
+      y: nodeIdMap[targetNodeId].y
+    }
+    const endPoint = {
+      x: nodeIdMap[targetNodeId].x - tModel.width / 2,
+      y: nodeIdMap[targetNodeId].y
+    }
+    edge.startPoint = startPoint
+    edge.endPoint = endPoint
+    edge.pointsList = [
+      startPoint,
+      sJustPoint,
+      tJustPoint,
+      endPoint
+    ]
+  })
+
+  lf.render({ nodes, edges });
 });
 </script>
 <style scoped lang="less">
-//.container {
-//  width: 100%;
-//  height: 100%;
-//}
 </style>
