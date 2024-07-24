23ee3302c7d98a6ddea1bdf3dc652794eff2a6da
Chris <yuxiaohui0626@gmail.com>
2024-07-24 11:07:16 +0800

    fix(store/job)

--- src/store/job.ts
+++ src/store/job.ts
@@ -26,9 +26,9 @@ export const useJobStore = defineStore("job", () => {
     }
   }
 
-  const breakdownData = computed(() => job2G6TreeGraph(_job.value));
+  const breakdownData = computed(() => job2G6TreeGraph(_job?.value));
 
-  const workflowData = computed(() => jobLeafNode2G6Graph(_job.value));
+  const workflowData = computed(() => jobLeafNode2G6Graph(_job?.value));
 
   return { updateData, breakdownData, workflowData };
 });
